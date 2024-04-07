93eb4a0377ddb40398ee8db3fe251c6fa5fa2954
jszafran <jszafran.pv@gmail.com>
2024-04-07 11:28:45 +0200

    Add loguru & create Eurostat db on app startup.

--- src/fastapi_eurostat_weekly_deaths/main.py
+++ src/fastapi_eurostat_weekly_deaths/main.py
@@ -1,14 +1,26 @@
 import pathlib
 
+import uvicorn
+from fastapi import FastAPI
+
 from fastapi_eurostat_weekly_deaths.eurostat import EurostatDB, FileEurostatData
 
+app = FastAPI()
 
-def main():
+
+@app.get("/")
+async def index():
+    return {"Hello": "world"}
+
+
+@app.on_event("startup")
+def eurostat_database() -> None:
+    print("Starting database")
     test_file = pathlib.Path(__file__).parent.parent.parent / "test_data" / "20240401.tsv"
     data_source = FileEurostatData(test_file)
     db = EurostatDB.from_data_source(data_source)
-    print(db.data)
+    app.eurostat_db = db
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app, port=8888)
